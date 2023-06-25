9e9c1514ce49876e39fd3d88e5a611358b98c9c4
markaz mis <mis.it.fava@gmail.com>
2023-06-25 09:35:54 +0330

    speed up sending sms

--- app/Http/Controllers/SMSController.php
+++ app/Http/Controllers/SMSController.php
@@ -13,12 +13,12 @@ class SMSController extends Controller
     public function sendSms()
     {
 
-        $send_req_count =  0;
+        $send_req_count = 0;
         $for_counter = 0;
 
-        for ($i = 1; $i < 4; $i++){
+        for ($i = 1; $i < 4; $i++) {
             $result = true;
-            while ($result){
+            while ($result) {
                 $result = $this->peyk();
                 sleep(0.5);
                 $send_req_count++;
@@ -55,53 +55,56 @@ class SMSController extends Controller
     }
 
 
-
-
-
     public function sendSingleResultSms()
     {
 
-        $last_send_message = SmsLog::orderBy('id', 'DESC')->first() ?? null;
+        for ($i = 0; $i < 20; $i++) {
 
-        if ($last_send_message == null){
-            $single_result = SingleResult::first();
-        }else{
-            $last_send_id = $last_send_message->ref_id;
-            $single_result = SingleResult::where('id', '>', $last_send_id)->first();
-        }
 
-        if ($single_result != null){
+            $last_send_message = SmsLog::orderBy('id', 'DESC')->first() ?? null;
 
-            $major = Major::find($single_result->major)->name ?? "قرآن";
-            $message = 'شرکت کننده مسابقات قران، '. $single_result->name .' عزیز' . "\r\n";
-            $message = $message . "شما در سامانه مسابقات در بخش فردی و در رشته ". str_replace(",","",$major) ."، ثبت نام کرده اید. این پیام صرفا جهت یادآوری می باشد. تمامی اطلاع رسانی های بعدی از طریق پیامک به شما اطلاع رسانی خواهد گردید.";
+            if ($last_send_message == null) {
+                $single_result = SingleResult::first();
+            } else {
+                $last_send_id = $last_send_message->ref_id;
+                $single_result = SingleResult::where('id', '>', $last_send_id)->first();
+            }
 
-            $sms_log = SmsLog::create([
-                'ref_id' => $single_result->id,
-                'ref_type' => 1,
-                'is_sended' => 0,
-                'message' => $message
-            ]);
+            if ($single_result != null) {
 
-            //send message
-            $sms_sender = new SmsSenderHelper();
+                $major = Major::find($single_result->major)->name ?? "قرآن";
+                $message = 'شرکت کننده مسابقات قران، ' . $single_result->name . ' عزیز' . "\r\n";
+                $message = $message . "شما در سامانه مسابقات در بخش فردی و در رشته " . str_replace(",", "", $major) . "، ثبت نام کرده اید. این پیام صرفا جهت یادآوری می باشد. تمامی اطلاع رسانی های بعدی از طریق پیامک به شما اطلاع رسانی خواهد گردید.";
 
-            $result = true;
-            while ($result){
-                $result = $sms_sender->peyk($message, $single_result->phone);
-                if (!$result){
-                    //here message sended
-                    $sms_log->update([
-                        'is_sended' => 1
-                    ]);
+                $sms_log = SmsLog::create([
+                    'ref_id' => $single_result->id,
+                    'ref_type' => 1,
+                    'is_sended' => 0,
+                    'message' => $message
+                ]);
 
-                    echo "message sended!";
+                //send message
+                $sms_sender = new SmsSenderHelper();
+
+                $result = true;
+                while ($result) {
+                    $result = $sms_sender->peyk($message, $single_result->phone);
+                    if (!$result) {
+                        //here message sended
+                        $sms_log->update([
+                            'is_sended' => 1
+                        ]);
+
+
+                    }
+                    sleep(0.5);
                 }
-                sleep(0.5);
+
             }
 
         }
 
+        echo "message sended!";
 
     }
 
